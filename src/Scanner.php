80e2824328712afbdf6ef6a8952d6353cc77b540
Alexander Hutorezki <olekhy@users.noreply.github.com>
2018-02-10 11:25:53 +0100

    Update Scanner.php
    Try to improve Scanner for easy to test and use this one

--- src/Scanner.php
+++ src/Scanner.php
@@ -8,36 +8,22 @@ use FilterIterator;
 
 class Scanner extends FilterIterator
 {
-    public function __construct(string $path)
+    public function __construct(string $path, CreateScannerIteratorStrategy $strategy)
     {
-        if (is_file($path)) {
-            if (! is_readable($path)) {
-                throw new \RuntimeException('path is not readable');
-            }
-
-            $iterator = new \ArrayIterator();
-            $iterator->append(new \SplFileInfo($path));
-        } elseif (is_dir($path)) {
-            $iterator = new \RecursiveDirectoryIterator($path);
+        $this->strategy = $strategy;
+        
+        if (is_readable($path)) {
+            $iterator = $strategy->createIterator($path);
+            parent::__construct($iterator);        
         } else {
-            throw new \RuntimeException('path is not a directory or file');
-        }
-
-        parent::__construct($iterator);
-    }
-
+            throw new \RuntimeException('Path: "' . $path . '" is not readable');     
+        }     
+    }    
+    
     public function accept()
     {
         $file = $this->getInnerIterator()->current();
-
-        if (! $file instanceof \SplFileInfo) {
-            return false;
-        }
-
-        if (! $file->isFile()) {
-            return false;
-        }
-
-        return $file->getExtension() === 'fpp';
+        
+        return $file->isFile() && $file->getExtension() === 'fpp' ? true : false;
     }
 }
