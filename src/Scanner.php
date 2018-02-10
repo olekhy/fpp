0935c6557bb572ccadf6832c90e40406a85da32d
Alexander Hutorezki <olekhy@users.noreply.github.com>
2018-02-10 11:38:15 +0100

    Use only easy dir or file iterator that was created before Scanner

--- src/Scanner.php
+++ src/Scanner.php
@@ -8,13 +8,12 @@ use FilterIterator;
 
 class Scanner extends FilterIterator
 {
-    public function __construct(string $path, CreateScannerIteratorStrategy $strategy)
+    public function __construct(string $path, Iterator $fileOrDirIterator)
     {
         $this->strategy = $strategy;
         
         if (is_readable($path)) {
-            $iterator = $strategy->createIterator($path);
-            parent::__construct($iterator);        
+            parent::__construct($fileOrDirIterator);        
         } else {
             throw new \RuntimeException('Path: "' . $path . '" is not readable');     
         }     
